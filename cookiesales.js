ff499ee9e41021e2c10ae6e354f42cfa4ef33da2
JPGothro <jpgothro@gmail.com>
2016-08-23 16:08:56 -0700

    organized js code

--- cookiesales.js
+++ cookiesales.js
@@ -57,63 +57,93 @@ Store.prototype.createTableData = function () {
 
   this.tableRow = bodyRow;
 };
+// ------------- THAT IS ALL THE STORE OBJECT STUFF
 
+// ------------- Now a couple general functions
+
+// create the header and append it to the passed table
+function createHdr(theTable) {
+  // create table header here
+  var hoursOpenArray = [];
+  var hour = 0;
+  var hourText = '';
+  for (var h = 0; h < 14; h++) {
+    hour = h + 6;
+    if (hour > 12) {
+      hourText = (hour - 12) + ':00 pm';
+    } else if ( hour < 12) {
+      hourText = hour + ':00 am';
+    } else {
+      hourText = hour + ':00 pm';
+    };
+    hoursOpenArray[h] = hourText;
+  };
+
+  var salesHeader = document.createElement('thead');
+  var headerRow   = document.createElement('tr');
+  var blankHdr    = document.createElement('th');
+  // we need a blank column heading before adding the rest of the header data
+  headerRow.appendChild(blankHdr);
+
+  for (var i = 0; i < hoursOpenArray.length; i++) {
+    var th = document.createElement('th');
+    th.textContent = hoursOpenArray[i];
+    headerRow.appendChild(th);
+  }
+  // we also need a final column for the total for the store
+  // we will just re-use the blankHdr we have sitting around.
+  var headerTotal = document.createElement('th');
+  headerTotal.textContent = 'Total';
+  headerRow.appendChild(headerTotal);
+
+  salesHeader.appendChild(headerRow);
+  theTable.appendChild(salesHeader);
+};
+
+// create the footer and append it to the passed table.
+function createFtr (theTable) {
+  // TODO: work the details out for the footer
+  var salesFoot = document.createElement('tfoot');
+  var footRow  = document.createElement('tr');
+  var footHdr   = document.createElement('th');
+  footHdr.textContent = 'TOTALS:';
+  footRow.appendChild(footHdr);
+
+  // load zeros in for totals for now.
+  for (var f = 0; f < footTotals.length; f++) {
+    var footData = document.createElement('td');
+    footData.textContent = footTotals[f];
+    footRow.appendChild(footData);
+  };
+  // add one more for the total column
+  var tdTotal = document.createElement('td');
+  tdTotal.textContent = footGrandTotal;
+  footRow.appendChild(tdTotal);
+
+  // put that footer row in the footer.
+  salesFoot.appendChild(footRow);
+  // and put the footer into the table.
+  theTable.appendChild(salesFoot);
+};
+// ------------ Done with general functions
+
+// Now run the 'control' script
 
 var allStores = [];
 
-var firstAndPike = new Store('1st and Pike', 23, 65, 6.3);
-var seaTac = new Store('SeaTac Airport', 3, 24, 1.2);
-var seaCtr = new Store('Seattle Center', 11, 28, 3.7);
-var capHill = new Store('Capitol Hill', 20, 38, 2.3);
-var alki = new Store('Alki', 2, 16, 4.6);
+allStores[0] = new Store('1st and Pike', 23, 65, 6.3);
+allStores[1] = new Store('SeaTac Airport', 3, 24, 1.2);
+allStores[2] = new Store('Seattle Center', 11, 28, 3.7);
+allStores[3] = new Store('Capitol Hill', 20, 38, 2.3);
+allStores[4] = new Store('Alki', 2, 16, 4.6);
 
-allStores[0] = firstAndPike;
-allStores[1] = seaTac;
-allStores[2] = seaCtr;
-allStores[3] = capHill;
-allStores[4] = alki;
-
-// create table header here
 // create the table to hold that data just created
 var salesTable = document.createElement('table');
+createHdr(salesTable);
 
-// create Header data here
-var hoursOpenArray = [];
-var hour = 0;
-var hourText = '';
-for (var h = 0; h < 14; h++) {
-  hour = h + 6;
-  if (hour > 12) {
-    hourText = (hour - 12) + ':00 pm';
-  } else if ( hour < 12) {
-    hourText = hour + ':00 am';
-  } else {
-    hourText = hour + ':00 pm';
-  };
-  hoursOpenArray[h] = hourText;
-};
-
-var salesHeader = document.createElement('thead');
-var headerRow   = document.createElement('tr');
-var blankHdr    = document.createElement('th');
-// we need a blank column heading before adding the rest of the header data
-headerRow.appendChild(blankHdr);
-
-for (var i = 0; i < hoursOpenArray.length; i++) {
-  var th = document.createElement('th');
-  th.textContent = hoursOpenArray[i];
-  headerRow.appendChild(th);
-}
-// we also need a final column for the total for the store
-// we will just re-use the blankHdr we have sitting around.
-var headerTotal = document.createElement('th');
-headerTotal.textContent = 'Total';
-headerRow.appendChild(headerTotal);
-
-salesHeader.appendChild(headerRow);
-salesTable.appendChild(salesHeader);
-
-var salesBody   = document.createElement('tbody');
+var salesBody  = document.createElement('tbody');
+// these two variables below are used when creating the footer, but populated
+// as eah table is processed.
 var footTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var footGrandTotal = 0;
 // now for each store, get it's data
@@ -131,28 +161,8 @@ for (var m = 0; m < allStores.length; m++) {
 // put the body into the table
 salesTable.appendChild(salesBody);
 
-// TODO: work the details out for the footer
-var salesFoot = document.createElement('tfoot');
-var footRow  = document.createElement('tr');
-var footHdr   = document.createElement('th');
-footHdr.textContent = 'TOTALS:';
-footRow.appendChild(footHdr);
-
-// load zeros in for totals for now.
-for (var f = 0; f < footTotals.length; f++) {
-  var footData = document.createElement('td');
-  footData.textContent = footTotals[f];
-  footRow.appendChild(footData);
-};
-// add one more for the total column
-var tdTotal = document.createElement('td');
-tdTotal.textContent = footGrandTotal;
-footRow.appendChild(tdTotal);
-
-// put that footer row in the footer.
-salesFoot.appendChild(footRow);
-// and put the footer into the table.
-salesTable.appendChild(salesFoot);
+// add the footer
+createFtr(salesTable);
 
 var storeData = document.getElementById('store_data');
 storeData.appendChild(salesTable);
